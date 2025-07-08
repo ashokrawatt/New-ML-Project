8baeeb97d4fa8b85cea71548a1acc5afa851bc06
Alok Rawat <alokrawatt@gmail.com>
2025-07-08 11:31:43 +0530

    eda & feature engineering

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,25 @@
 import sys 
-import logging
+from src.logger import logging
 
 
-def error_massege_detail(error, error_detail:sys):
-    _,_,exe_tb = error_detail.exe_info()
-    file_name = exe_tb.tb_frame.f_code.co_filename
-    error_massege = "error occured in python script name [{0}] line number [{1}] error massege [{2}]" .format(
-     file_name,exe_tb.tb_lineno,str(error)
+def error_message_detail(error, error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "error occured in python script name [{0}] line number [{1}] error message [{2}]" .format(
+        file_name,exc_tb.tb_lineno,str(error))
     
-    )
     
-    return error_massege
+    return error_message
     
     
 class CustomException(Exception):
-    def __init__(self,error_massege, error_detail:sys):
-        super().__init__(error_massege)
-        self.error_massege=error_massege_detail(error_massege,error_detail=error_detail)
+    def __init__(self,error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_massege=error_message_detail(error_message,error_detail=error_detail)
         
         
     def __str__(self):
-        return self.error_massege
+        return self.error_message
     
     
 if __name__=="__main__":
@@ -28,8 +27,8 @@ if __name__=="__main__":
     try:
         a= 1/0
     except Exception as e:
-        logging.info("devide by zero")
+        logging.info("divide by zero")
         raise CustomException(e,sys)
      
      
-     
+      
